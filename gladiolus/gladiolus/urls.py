e6e5f3e75368b27825c61f7222fba6f4f2253263
Nikita Rotsky <kurt.rotzke@gmail.com>
2018-01-24 17:33:40 +0200

    Added header

--- gladiolus/gladiolus/urls.py
+++ gladiolus/gladiolus/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^1/', include('page1.urls')),
+    url(r'^', include('page1.urls')),
 ]
